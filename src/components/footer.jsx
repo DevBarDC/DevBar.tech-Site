c17c52e8b7be9fa58a5f158b7ac1f9d33ce4d438
Tyler <102545881+CodedByTy@users.noreply.github.com>
2025-06-08 00:40:38 +0200

    Updated Footer

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,25 +1,27 @@
 import React from "react";
-import { FaFacebook, FaTwitter, FaInstagram } from "react-icons/fa";
+import { FaPatreon, FaGithub, FaDiscord } from "react-icons/fa";
 import "./css/footer.css"; 
 
 function Footer() {
     return (
-        <footer className="footer">
-            <div className="footer-content">
-                <p>&copy; 2023 My Website. All rights reserved.</p>
-                <div className="social-icons">
-                    <a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer">
-                        <FaFacebook />
-                    </a>
-                    <a href="https://www.twitter.com" target="_blank" rel="noopener noreferrer">
-                        <FaTwitter />
-                    </a>
-                    <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer">
-                        <FaInstagram />
-                    </a>
+        <div className="footer-section-start">
+            <footer className="footer">
+                <div className="footer-content">
+                    <p>&copy; 2025 DevBar. All rights reserved.</p>
+                    <div className="social-icons">
+                        <a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer">
+                            <FaDiscord />
+                        </a>
+                        <a href="https://www.twitter.com" target="_blank" rel="noopener noreferrer">
+                            <FaGithub />
+                        </a>
+                        <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer">
+                            <FaPatreon />
+                        </a>
+                    </div>
                 </div>
-            </div>
-        </footer>
+            </footer>
+        </div>
     );
 }
 export default Footer;
