1aaa08b86c6525feb429e491157e3a49668b3411
Tyler <102545881+CodedByTy@users.noreply.github.com>
2025-06-07 21:48:49 +0200

    Initialize Vite
    Initialized the repository.

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import {  } from "react-icons/fa";
+import { Header, Footer } from './components/index.js';
+import './global.css'
+
+function App() {
+  return (
+    <>
+      <div>
+        <main className="main-content">
+          <Header />
+          <Footer />
+        </main>
+      </div>
+    </>
+  )
+}
+
+export default App
