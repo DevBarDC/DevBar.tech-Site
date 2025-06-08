49fc73b31451206443816f66063ca9ca810023e0
Tyler <102545881+CodedByTy@users.noreply.github.com>
2025-06-08 21:15:55 +0200

    chore: update dependencies and configure Tailwind CSS
    - Added Tailwind CSS and its Vite plugin to dependencies.
    - Updated global CSS to use Roboto font instead of Meow Script.
    - Configured Vite to include Tailwind CSS in the build process.

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    tailwindcss(),
+    react()
+  ],
   base: process.env.BASE_PATH || '/', // Use environment variable from workflow or fallback to '/'
 });
