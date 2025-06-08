a4a0bb721b9a263465378076e4ebd2f2bdb0e44b
Tyler <102545881+CodedByTy@users.noreply.github.com>
2025-06-08 22:34:05 +0200

    Uninstalled TailwindCSS

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [
-    tailwindcss(),
     react()
   ],
   base: process.env.BASE_PATH || '/', // Use environment variable from workflow or fallback to '/'
